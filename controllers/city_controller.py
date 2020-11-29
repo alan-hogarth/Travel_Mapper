875406a5e6b08be74eaa3fa1c93407f149b525fd
Alan Hogarth <alan.hogarth@outlook.com>
2020-11-29 12:32:15 +0000

    add city function

--- controllers/city_controller.py
+++ controllers/city_controller.py
@@ -11,15 +11,24 @@ def cities():
     cities = city_repository.select_all() 
     return render_template("cities/index.html", cities = cities)
 
-# @cities_blueprint.route("/cities/<id>")
-# def show_country(id):
-#     city = city_repository.select(id)
-#     city_countries = city_repository.countries(city)
-#     return render_template("cities/index.html", cities=city, countries=city_countries)
-
 
 @cities_blueprint.route("/cities/<id>")
 def show(id):
     city = city_repository.select(id)
     city_users = city_repository.users(city)
     return render_template("cities/show.html", city=city, users=city_users)
+    
+@cities_blueprint.route("/cities", methods=["POST"])
+def create_city():
+    city = request.form["city"]
+    new_city = City(city)
+    city_repository.save(new_city)
+    return redirect("/cities/index.html")
+
+
+    
+    # @cities_blueprint.route("/cities/<id>")
+# def show_country(id):
+#     city = city_repository.select(id)
+#     city_countries = city_repository.countries(city)
+#     return render_template("cities/index.html", cities=city, countries=city_countries)
--- controllers/visit_controller.py
+++ controllers/visit_controller.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.visit import Visit
+from models.city import City
+from models.country import Country
+from models.user import User
+
 import repositories.visit_repository as visit_repository
 import repositories.user_repository as user_repository
 import repositories.city_repository as city_repository
@@ -20,21 +24,22 @@ def new_trip():
     country = country_repository.select_all()
     return render_template("trips/new.html", users = users, city = city, country = country)
 
-@trips_blueprint.route("/trips",  methods=['POST'])
-def create_trip():
-    user_id = request.form['user_id']
-    city_id = request.form['city_id']
-    country_id = request.form['country_id']
-    to_visit = request.form['to_visit']
-    user = user_repository.select(user_id)
-    city = city_repository.select(city_id)
-    country = country_repository.select(country_id)
-    visit = Visit(user, city, country, to_visit)
-    visit_repository.save(visit)
+# @trips_blueprint.route("/trips",  methods=['POST'])
+# def create_trip():
+#     user = request.form["name"]
+#     country = request.form["country"]
+#     city = request.form["city"]
+#     new_user = User(user)
+#     new_country = Country(country) 
+#     new_city = City(city)
+#     trip = Visit(new_user, new_country, new_city)
+#     visit_repository.save(trip)
+#     return redirect("/trips")
     
-    return redirect('/trips')
-
 @trips_blueprint.route("/trips/<id>/delete", methods=["POST"])
 def delete_trip(id):
     visit_repository.delete(id)
     return redirect("/trips")
+
+
+
