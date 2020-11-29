43377e5523a056a29a788503553b5f3c1a433266
Alan Hogarth <alan.hogarth@outlook.com>
2020-11-29 14:59:30 +0000

    work on country update function

--- controllers/country_controller.py
+++ controllers/country_controller.py
@@ -12,7 +12,7 @@ def countries():
     return render_template("countries/index.html", countries = countries)
 
 @countries_blueprint.route("/countries/<id>")
-def show_city(id):
+def show_country(id):
     country = country_repository.select(id)
     country_cities = country_repository.cities(country)
     return render_template("countries/show.html", country=country, cities=country_cities)
@@ -24,3 +24,17 @@ def show_user(id):
     return render_template("countries/show.html", country=country, users=country_users)
 
 
+# EDIT
+@countries_blueprint.route("/countries/<id>/edit")
+def edit_country(id):
+    country = country_repository.select(id)
+    return render_template("countries/edit.html", country=country)
+
+
+# UPDATE
+@countries_blueprint.route("/countries/<id>", methods=["POST"])
+def update_country(id):
+    name = request.form["name"]
+    country = Country(name, id)
+    country_repository.update(country)
+    return redirect("/countries")
